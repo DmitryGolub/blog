49174bd13beb0b9b36c2697684944a2e79eebb00
Dmitry Golub <gosest4@gmail.com>
2025-03-21 23:23:11 +0300

    Add config file

---
+++ config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+    DB_PORT: int
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgres+asyncp://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}"
+    
+    class Config:
+        env_file = ".env"
+
+
+setting = Settings()
