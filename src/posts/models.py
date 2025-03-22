294935e5733550cd49ae0f390b3acebf183ac7eb
Dmitry Golub <gosest4@gmail.com>
2025-03-22 10:14:07 +0300

    Create comments and posts models

---
+++ src/posts/models.py
@@ -0,0 +1,15 @@
+from uuid import uuid4
+from sqlalchemy import Column, ForeignKey, UUID, String, Integer
+from sqlalchemy.orm import relationship
+from src.database import Base
+
+
+class Posts(Base):
+    __tablename__ = "posts"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    title = Column(String(256), nullable=False)
+    text = Column(String, nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
+
+    user = relationship("Users", back_populates="post")
--- src/users/models.py
+++ src/users/models.py
@@ -1,12 +1,16 @@
-import uuid
-from sqlalchemy import Column, DateTime, Integer, ForeignKey, String
+from uuid import uuid4
+from sqlalchemy import Column, String
 from src.database import Base
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 
 
 class Users(Base):
     __tablename__ = "users"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     username = Column(String(50), nullable=False)
     hashed_password = Column(String, nullable=False)
+
+    post = relationship("Posts", back_populates="user")
+    comment = relationship("Comments", back_populates="user")
