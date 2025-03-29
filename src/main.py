0735baa71e9f0bfead99408d8dbe765f68b98f84
Dmitry Golub <gosest4@gmail.com>
2025-03-30 00:54:13 +0300

    Remove pages and templates

--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ from src.posts.router import router as posts_router
 from src.users.router import router as users_router
 from src.posts.comments.router import router as comments_router
 
-from src.pages.router import router as pages_router
 from src.images.router import router as router_images
 
 
@@ -18,5 +17,4 @@ app.mount("/static", StaticFiles(directory="src/static"), "static")
 app.include_router(posts_router)
 app.include_router(users_router)
 app.include_router(comments_router)
-app.include_router(pages_router)
 app.include_router(router_images)
