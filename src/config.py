df9c1527474c0eebb5c77c416ce7a05fdf1be08d
Dmitry Golub <gosest4@gmail.com>
2025-04-05 21:03:25 +0300

    Add enviroment for connection to db in test mode

--- src/config.py
+++ src/config.py
@@ -1,12 +1,30 @@
 from pydantic_settings import BaseSettings
+from typing import Literal
 
 
 class Settings(BaseSettings):
+    MODE: Literal["DEV", "TEST", "PROD"]
+
     DB_HOST: str
     DB_USER: str
     DB_PASS: str
     DB_NAME: str
     DB_PORT: int
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    
+    TEST_DB_HOST: str
+    TEST_DB_USER: str
+    TEST_DB_PASS: str
+    TEST_DB_NAME: str
+    TEST_DB_PORT: int
+
+    @property
+    def TEST_DATABASE_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
+    
 
     SECRET_KEY: str
     ALGORITHM: str
@@ -19,10 +37,6 @@ class Settings(BaseSettings):
     REDIS_HOST: str
     REDIS_PORT: int
  
-    @property
-    def DATABASE_URL(self) -> str:
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-    
     class Config:
         env_file = ".env"
 
